1d22139de5ecb6514b54fa44e8fc7633fcdf43c6
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-04 11:54:17 +0200

    snack 1

---
+++ snack-1/main.js
@@ -0,0 +1,11 @@
+firstNumber = prompt ("inserisci il primo numero");
+secondNumber = prompt ("inserisci il secondo numero");
+
+if(firstNumber>secondNumber) {
+    alert(firstNumber);
+}
+else if(firstNumber<secondNumber) {
+    alert(secondNumber);
+} else {
+    alert("sono uguali");
+}
