62e0902404760f8265a4d5b0fd0b08845b568126
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-04 11:49:55 +0200

    second snack

---
+++ snack-2/main.js
@@ -0,0 +1,11 @@
+firstWord = prompt("inserisci la prima parola");
+secondWord = prompt("inserisci la seconda parola");
+
+if(firstWord.length > secondWord.length) {
+    alert(secondWord);
+    alert(firstWord);
+}
+else {
+    alert(firstWord);
+    alert(secondWord);
+}
