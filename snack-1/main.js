339301574476aba813d081895bc87b28e06bfcbc
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-04 11:32:31 +0200

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,11 @@
+firstNumber = prompt("inserisci il primo numero");
+secondNumber = prompt("inserisci il secondo numero");
+
+if(firstNumber>secondNumber) {
+    alert(firstNumber);
+}
+else if(secondNumber>firstNumber) {
+    alert(secondNumber);
+} else {
+    alert("i numeri sono uguali");
+}
