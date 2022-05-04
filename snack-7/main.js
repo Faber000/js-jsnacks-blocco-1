ab5ce0799cdd90629d118140dc058d8667b790ca
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-04 16:49:08 +0200

    snack 7

---
+++ snack-7/main.js
@@ -0,0 +1,12 @@
+let nomi = ["Fabio", "Andrea", "Mario", "Luca", "Giovanni"];
+let cognomi = ["Moro", "Amoretti", "Di Salvo", "De Santis", "Auletta"];
+
+let invitato;
+let invitati = [];
+for (let i=0; i<5; i++) {
+    invitato = nomi[Math.floor(Math.random() * 5)] + " "+cognomi [Math.floor(Math.random() * 5)];
+    invitati.push(invitato);
+}
+alert(invitati);
+
+
